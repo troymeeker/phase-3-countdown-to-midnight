21fa70a0557c5d4dd384b25a69966b9a2a53d5d1
troymeeker <troymeeker1@gmail.com>
2021-12-14 21:21:34 -0800

    commit tree 2230745a0c8778ffc25c88b771e0d062dbd1aaec

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown n
+ while n > 0
+  puts "#{n} SECOND(S)!"
+  n -=1
+ end
+  "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep how_long = 5
+ sleep how_long
 end
